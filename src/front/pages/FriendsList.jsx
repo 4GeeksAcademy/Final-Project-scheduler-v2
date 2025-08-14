d7a292d3514803a59579f765fddc42c5a5333369
Nhcdw57 <chocolatedounut@gmail.com>
2025-08-14 00:10:30 +0000

    making first commit to friends list page

---
+++ src/front/pages/FriendsList.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+
+export const FriendsList = () => {
+
+    //insert some bs about aking a fetch call to the Database for a list of user's friends (probably a protected view)
+    const [friendsList,setFriendsList] = useState([]);
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div className="text-center mt-5">
+            <h1>HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</h1>
+        </div>
+    );
+}; 
