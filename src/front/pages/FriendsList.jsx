e7986612bf3a89aa12658f7f785b9abafac391da
Nhcdw57 <chocolatedounut@gmail.com>
2025-08-14 00:44:04 +0000

    added bare minimum to the friends list page

--- src/front/pages/FriendsList.jsx
+++ src/front/pages/FriendsList.jsx
@@ -4,15 +4,54 @@ import { useEffect, useState } from "react";
 export const FriendsList = () => {
 
     //insert some bs about aking a fetch call to the Database for a list of user's friends (probably a protected view)
-    const [friendsList,setFriendsList] = useState([]);
+    const [friendsList, setFriendsList] = useState([
+        {
+            "FirstName": "Bob",
+            "LastName": "The Builder"
+        },
+        {
+            "FirstName": "Barney",
+            "LastName": "The Purple Dinosaur"
+        }
+    ]); // making fake friends objects
 
     useEffect(() => {
 
     }, [])
 
+    let listedFriends = friendsList.map((friend) => (
+        <tr>
+            <td>{`${friend["FirstName"]} ${friend["LastName"]}`}</td>
+            <td>View Profile</td>
+            <td>Remove</td>
+        </tr>));
+
     return (
-        <div className="text-center mt-5">
-            <h1>HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</h1>
+        <div className="container-fluid bg-success">
+            <div className="row d-flex justify-content-center">
+                <div className="col-5 mt-5">
+                    <div className="input-group mb-3 mx-5">
+                        <span className="input-group-text" id="basic-addon1">Search</span>
+                        <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                    </div>
+                </div>
+            </div>
+            <div className="row d-flex justify-content-center">
+                <div className="col-4 mt-3 mb-5">
+                    <table className="table table-dark table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">View</th>
+                                <th scope="col">Remove</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {listedFriends}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }; 
