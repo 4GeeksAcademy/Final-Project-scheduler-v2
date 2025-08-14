f20690a7af1f28b5d49b79a485442d0563a576bb
Nhcdw57 <chocolatedounut@gmail.com>
2025-08-14 00:50:05 +0000

    fixed minor misalignment with search bar

--- src/front/pages/FriendsList.jsx
+++ src/front/pages/FriendsList.jsx
@@ -30,7 +30,7 @@ export const FriendsList = () => {
         <div className="container-fluid bg-success">
             <div className="row d-flex justify-content-center">
                 <div className="col-5 mt-5">
-                    <div className="input-group mb-3 mx-5">
+                    <div className="input-group mb-3">
                         <span className="input-group-text" id="basic-addon1">Search</span>
                         <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
                     </div>
