80b8ab6fae90216df34f267209d827c814c1e630
Haley <haleymariehernandez10@gmail.com>
2025-08-19 00:11:53 +0000

    created the footer

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,10 @@
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3 text-center bg-light">
+		<div>
+			<strong>Time Tidy</strong> – Stay organized, stay on time.
+		</div>
+		<div className="mt-2 small text-muted">
+			© 2025 Time Tidy. All rights reserved.
+		</div>
 	</footer>
 );
