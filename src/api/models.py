89795cf1de16674aa02c10de5eee0e503c892dc0
Nhcdw57 <chocolatedounut@gmail.com>
2025-08-22 22:26:53 +0000

    filled out most of the things that are present without user things for the backend, also need to consult about how the events data is gonna be stored

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,46 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Events(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    date: Mapped[str] = mapped_column(unique=False, nullable=False)
+    time = Mapped[str] = mapped_column(unique=False, nullable=False)
+    timezone = Mapped[str] = mapped_column(unique=False, nullable=False)
+    attendees = Mapped[str] = mapped_column(unique=False, nullable=False)
+    visibility = Mapped[str] = mapped_column(unique=False, nullable=False)
+    host = Mapped[str] = mapped_column(unique=False, nullable=False)
+    repeat = Mapped[str] = mapped_column(unique=False, nullable=False)
+    description = Mapped[str] = mapped_column(unique=False, nullable=False)
+    goalAmount = Mapped[str] = mapped_column(unique=False, nullable=False)
+    timer = Mapped[str] = mapped_column(unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "time": self.time,
+            "timezone": self.timezone,
+            "attendees": self.attendees,
+            "visibility": self.visibility,
+            "host": self.host,
+            "repeat": self.repeat,
+            "description": self.description,
+            "goalAmount": self.goalAmount,
+            "timer": self.timer
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Events
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -20,3 +20,19 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/createEvent', methods=['POST'])
+def post_event_route():
+
+    # do updating in the database
+    request_data = request.json
+    user_id = request_data["host"]
+    # user = User.query.get(user_id)
+    new_event = Events(
+        # fill all this in with the data needed to create an event
+    )
+    # db.session.add(new_event)
+    # db.session.commit()
+
+    return jsonify("ok"), 200
