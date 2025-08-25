173dde1219e04e42f9dc32e09746da67232dfd4c
Nhcdw57 <chocolatedounut@gmail.com>
2025-08-25 15:33:22 +0000

    merge conflict fix

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -8,13 +8,9 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
-<<<<<<< HEAD
+import { Signup } from "./pages/Signup";
 import { FavoritesList } from "./pages/FavoritesList";
 import ProfilePage from "./pages/Profile-page";
-=======
-import { Signup } from "./pages/Signup";           
-import ProfilePage from "./pages/Profile-page";     
->>>>>>> 2383b475a84be406731fe51c001282829bf2a0c3
 import Login from "./pages/Login.jsx";
 
 export const router = createBrowserRouter(
@@ -29,13 +25,9 @@ export const router = createBrowserRouter(
       <Route path="/profile/:userId" element={<ProfilePage />} />
       <Route path="/single/:theId" element={<Single />} />
       <Route path="/demo" element={<Demo />} />
-<<<<<<< HEAD
       <Route path="/profile/:userId" element={<ProfilePage />} />
       <Route path="/favoritesList" element={<FavoritesList />} />
     </Route >
 
-=======
-    </Route>
->>>>>>> 2383b475a84be406731fe51c001282829bf2a0c3
   )
 );
