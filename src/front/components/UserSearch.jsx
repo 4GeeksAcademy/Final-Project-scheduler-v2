6efcea2c7c24ba9d7104dee95f1b9a5438ad0847
Nhcdw57 <chocolatedounut@gmail.com>
2025-09-03 17:15:48 +0000

    added a user search component, is functional, but needs prettying

--- src/front/components/UserSearch.jsx
+++ src/front/components/UserSearch.jsx
@@ -1,43 +1,76 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+const API_URL = import.meta.env.VITE_BACKEND_URL
+
 export function UserSearch() {
+
+    const [input, setInput] = useState("");
+    const [result, setResult] = useState([]);
+    const [searched, setSearched] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    async function searchFunction(e) {
+        e.preventDefault();
+        setLoading(true)
+        const rawResponse = await fetch(`${API_URL}api/search/${input}`);
+        let data = await rawResponse.json();
+        setResult(data["search_results"]);
+        setLoading(false)
+        setSearched(true)
+    }
+
+    useEffect(() => { if (input === "") { setSearched(false) } }, [result]);
+
+    let resultWindow = result.map((user, index) => (
+        <div key={index} className="card p-5 shadow-sm rounded-3 mb-3" style={{ maxWidth: "500px", width: "70%" }}>
+            <Link to={`/profile/${user.id}`}><span>{user.username}</span></Link>
+
+        </div>
+    ));
+
     return (
-        <div className="d-flex align-items-center justify-content-center min-vh-100 bg-light p-3">
-            <div className="card p-5 shadow-sm rounded-3" style={{ maxWidth: "400px", width: "100%" }}>
+        <div className="mt-5 d-flex align-items-center justify-content-center min-vh-100 bg-light p-3 flex-column">
+            <div className="card p-5 shadow-sm rounded-3" style={{ maxWidth: "500px", width: "100%" }}>
                 <div className="text-center">
-                    <h2 className="fw-bold">Sign In</h2>
-                    <p className="text-muted">Sign in to continue</p>
-                    <hr className="my-4" />
+                    <h2 className="fw-bold mb-5">User Search</h2>
                 </div>
 
-                <form>
-                    <div className="mb-4">
-                        <label htmlFor="usernameInput" className="form-label">Username:</label>
+                <form onSubmit={searchFunction}>
+                    <label className="form-label">Username:</label>
+                    <div className="mb-4 d-flex flex-row">
+
                         <input
-                            id="usernameInput"
+                            id="searchInput"
                             type="text"
                             className="form-control rounded-pill"
                             autoComplete="username"
-                            required
+                            value={input}
+                            onChange={(event) => setInput(event.target.value)}
                         />
+                        <button type="submit" className="ms-3 btn btn-dark rounded-pill">
+                            Search
+                        </button>
                     </div>
-
-                    <div className="mb-4">
-                        <label htmlFor="passwordInput" className="form-label">Password:</label>
-                        <input
-                            id="passwordInput"
-                            type="password"
-                            className="form-control rounded-pill"
-                            autoComplete="current-password"
-                            required
-                        />
-                    </div>
-
 
                 </form>
 
 
+
             </div>
+            {(!searched) ? (<span></span>) : (
+                <div className="container d-flex justify-content-center min-vh-100 bg-light p-3">
+                    <div className="card p-5 shadow-sm rounded-3" style={{ maxWidth: "500px", width: "100%" }}>
+                        {(loading) ? (
+                            <div className="text-center my-5">
+                                <div className="spinner-border text-primary" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </div>
+                            </div>
+                        ) : resultWindow}
+                    </div>
+                </div>)}
+
         </div>
     );
 };
