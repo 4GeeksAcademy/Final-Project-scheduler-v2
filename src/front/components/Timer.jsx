ccfdbfd37401ec7b630ec4374640a67c6d58bb28
Nhcdw57 <chocolatedounut@gmail.com>
2025-09-09 00:23:59 +0000

    got the timer working

--- src/front/components/Timer.jsx
+++ src/front/components/Timer.jsx
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { NavbarContext } from "../hooks/NavbarContext";
 
 export default function Timer({ initialTime }) {
-    const [timeLeft, setTimeLeft] = useState(initialTime)
+    const [timeLeft, setTimeLeft] = useState(initialTime);
+    const { timerStart } = useContext(NavbarContext);
 
     useEffect(() => {
         const interval = setInterval(() => {
+
             setTimeLeft(prevTime => {
-                if (prevTime > 0) {
-                    return prevTime - 1;
+                if (timerStart) {
+                    if (prevTime > 0) {
+                        return prevTime - 1;
+                    } else {
+                        return 0;
+                    }
                 } else {
-                    return 0;
+                    return prevTime
                 }
             });
         }, 1000);
 
         return () => clearInterval(interval);
-    }, []);
+    }, [timerStart]);
     const hours = Math.floor(timeLeft / 3600);
     const minutes = Math.floor(timeLeft / 60) % 60;
     const seconds = timeLeft % 60;
